89db851ad7c998c230290587644fcd42570ceb05
berkay oren <benberkayoren@gmail.com>
2022-09-19 23:10:40 +0300

    End of the lesson

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ const addBtn = document.getElementById("todo-button");
 const todoInput = document.getElementById("todo-input");
 const todoUl = document.getElementById("todo-ul");
 
-// todos dizisini localStorage'daki veriler ile günceele
-//! eger localStorage'de todos adinda bir item bulunmaz ise boş array atamasi yap
-
+// todos dizisini localStorage'daki veriler ile guncelle
+//! eger localStroge'de todos adinda bir item bulunmaz ise bos array atamasi yap.
 let todos = JSON.parse(localStorage.getItem("TODOS")) || [];
-
 console.log(todos);
 
 const renderSavedTodos = () => {
-  todos.array.forEach((todo) => {
+  todos.forEach((todo) => {
     createListElement(todo);
   });
 };
@@ -35,8 +33,9 @@ addBtn.addEventListener("click", () => {
     //! yeni bir li elementi olusturup bunu DOM'a bas
     createListElement(newTodo);
 
-    //? yeni olusturulan todo'yu diziye sakla
+    //?Yeni olusturulan todo'yu diziye sakla
     todos.push(newTodo);
+
     localStorage.setItem("TODOS", JSON.stringify(todos));
     console.log(todos);
     todoInput.value = "";
@@ -76,17 +75,26 @@ function createListElement(newTodo) {
   todoUl.appendChild(li);
 }
 
+//! Ul elementinin cocuklarindan herhangi birisinden bir event gelirse
+//! bunu tespit et ve gerekini yap. (Capturing)
 todoUl.addEventListener("click", (e) => {
   console.log(e.target);
 
+  const id = e.target.parentElement.getAttribute("id");
   //! event, bir delete butonundan geldi ise
   if (e.target.classList.contains("fa-trash")) {
+    //? delete butonunun parent'ini DOM'dan sil
     e.target.parentElement.remove();
-  }
 
-  //! event bir okay butonundan geldi ise
-  if (e.target.classList.contains("fa-check")) {
-    //? ilgili li elementinde checked adinda bir class'i varsa bunu sil aksi takdirde ekle (DOM)
+    //? Dizinin ilgili elementini sil
+    todos = todos.filter((todo) => todo.id !== Number(id));
+
+    //? todos dizisinin son halini localStorage'e sakla
+    localStorage.setItem("TODOS", JSON.stringify(todos));
+  } else if (e.target.classList.contains("fa-check")) {
+    //! event, bir okey butonundan geldi ise
+    //? ilgili li elementinde checked adinda bir class'i varsa bunu sil
+    //?  aksi takdirde ekle (DOM)
     e.target.parentElement.classList.toggle("checked");
   }
 });
